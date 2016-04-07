859f638dea061341e3d6b3d66b1c463b1c223f20
Jacek Mariański <marianski.jacek@gmail.com>
2016-04-07 13:22:49 +0200

    Testing out different options in curl

--- app/PHPHtmlParser/Curl.php
+++ app/PHPHtmlParser/Curl.php
@@ -23,10 +23,14 @@ class Curl implements CurlInterface
         $ch = curl_init($url);
 
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
-		curl_setopt($ch, CURLOPT_USERAGENT, "Googlebot-News");
+		curl_setopt($ch, CURLOPT_MAXREDIRS, 30);
+		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/32.0.1700.107 Chrome/32.0.1700.107 Safari/537.36");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_COOKIESESSION, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type:application/x-www-form-urlencoded"));
+		curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie-name');  //could be empty, but cause problems on some hosts
+		curl_setopt($ch, CURLOPT_COOKIEFILE, 'tmp/cookie');  //could be empty, but cause problems on some hosts
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANYSAFE);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
